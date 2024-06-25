f703770daaba984f0fe772a41dee01a151981e5e
Eadweard <aurora.coder.zqf@gmail.com>
2024-06-25 18:08:13 +0800

    finish

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,19 @@ function textHighlight(config: DeafultConfig) {
   const { text, keywords } = config
 
   if (typeof keywords === 'string') {
-
+    // 这里我希望大小写也兼容
+    const keywordsReg = `\\b${keywords}\\b`
+    const reg = new RegExp(keywordsReg, 'gi')
+    if (text.match(reg)) {
+      const result = text.replace(reg, `<span style="color: red;">${keywords}</span>`)
+      console.log(result)
+    }
+    else {
+      throw new Error('未匹配到关键字, 请检查内容')
+    }
   }
 
   if (Array.isArray(keywords)) {
-
   }
 }
 
