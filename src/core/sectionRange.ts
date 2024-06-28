ba2d685b1096dd4872727552ed0686e6462d69bd
Eadweard <aurora.coder.zqf@gmail.com>
2024-06-28 10:33:05 +0800

    sectionRange完善

--- src/core/sectionRange.ts
+++ src/core/sectionRange.ts
@@ -1,6 +1,6 @@
 import type { OffsetAndSetRange } from '../types'
 
-export default (container: Node, positions: { start: number, end: number }[], richText?: boolean) => {
+export default (container: Node, positions: { start: number, end: number }[]) => {
   // Selection 对象表示用户选择的文本范围或插入符号的当前位置。它代表页面中的文本选区，可能横跨多个元素。通常由用户拖拽鼠标经过文字而产生
   const selection = document?.getSelection()
   if (!selection) {
@@ -8,23 +8,13 @@ export default (container: Node, positions: { start: number, end: number }[], ri
   }
 
   selection.removeAllRanges()
-  if (richText) {
-    positions.forEach(({ start, end }) => {
-      const [startNode, startIndex, endNode, endIndex] = getNodeAndOffset(container, start, end) as OffsetAndSetRange
-      const range = document.createRange()
-      range.setStart(startNode, startIndex)
-      range.setEnd(endNode, endIndex)
-      selection.addRange(range)
-    })
-  }
-  else {
-    positions.forEach(({ start, end }) => {
-      const range = document.createRange()
-      range.setStart((container as any).firstChild, start)
-      range.setEnd((container as any).firstChild, end)
-      selection.addRange(range)
-    })
-  }
+  positions.forEach(({ start, end }) => {
+    const [startNode, startIndex, endNode, endIndex] = getNodeAndOffset(container, start, end) as OffsetAndSetRange
+    const range = document.createRange()
+    range.setStart(startNode, startIndex)
+    range.setEnd(endNode, endIndex)
+    selection.addRange(range)
+  })
 }
 
 /**
